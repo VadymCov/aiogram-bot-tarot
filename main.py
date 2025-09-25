51ac55555536d3209a2729f84cd3b6e58192a325
Vadym <vadymkravtcsov@gmail.com>
2025-09-26 00:11:43 +0300

    fix: refactor bot initialization and add callback handler

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import asyncio 
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 
 
-from database.database import engine
-from database.models import Base
+from database.database import init_db
 from bot.handlers.start import router as start_router
-
+from bot.handlers.callbacks import router as callback_router
 from config.settings import BOT_TOKEN 
 
 logging.basicConfig(level=logging.INFO)
@@ -14,19 +13,14 @@ logger = logging.getLogger(__name__)
 
 
 
-
-
-async def init_db():
+async def main():
     try:
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
+        await init_db()
         logging.info("✅ The database has been initialized")
     except Exception as e:
         logging.info("❌ Failed to initialize database")
         raise Exception(f"{e}")
 
-async def main():
-    await init_db()
 
     try:
         bot = Bot(token=BOT_TOKEN) # type: ignore
@@ -38,12 +32,13 @@ async def main():
 
     try:
         dp.include_router(start_router)
+        dp.include_router(callback_router)
         logging.info("✅ All handlers registered")
     except Exception as e:
         logging.info("❌ Failed to register handlers: {e}")
         return
 
-    logger.info("✅ The bot is starting")
+    logger.info("✅ The bot is starting")   
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
