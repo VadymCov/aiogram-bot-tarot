aec1b77236f249bfec56d4f4312a5f501d8126d1
Vadym <vadymkravtcsov@gmail.com>
2025-09-26 00:13:43 +0300

    fix: update start handler to support user language

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,12 +1,15 @@
 from aiogram import Router, types
 from aiogram.filters import Command
+from bot.keyboards.inline import get_language_keyboard
+from database.operations import get_or_create_user
 
-from languages import get_text
+from bot.locales.utils import get_text
 
 router = Router()
 
 @router.message(Command('start'))
 async def start_handler(message: types.Message):
-    await message.answer(get_text("welcome", )
-    )
-    pass
+    user =  await get_or_create_user(message.from_user.id) # type: ignore
+    await message.answer(get_text("welcome", user.language),
+        reply_markup=get_language_keyboard()
+        )
