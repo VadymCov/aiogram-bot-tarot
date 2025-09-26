d10bc24ad2b2ab8f4fea4b9e0930b00017a64a32
Vadym <vadymkravtcsov@gmail.com>
2025-09-26 23:36:56 +0300

    Revert "update file"
    This reverts commit faafcff748caeaf1972894d2e3506eb47d3127be.

--- database/database.py
+++ database/database.py
@@ -1,11 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from database.models import Base
-
 DATABASE_URL = "postgresql+asyncpg://user:pass@localhost/astro_bot"
 
 engine = create_async_engine(DATABASE_URL)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
