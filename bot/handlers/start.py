df272da52e5e0838958830208cb56f9272e1c22f
Vadym <vadymkravtcsov@gmail.com>
2025-09-28 23:53:16 +0300

    fix: update start handler to use JSON menu texts
    The start handler now loads menu texts from 'data/text_menu.json' and personalizes the welcome message using the user's first name. The message is sent with HTML parse mode and the language keyboard, improving localization and user experience.

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,15 +1,22 @@
 from aiogram import Router, types
 from aiogram.filters import Command
+
 from bot.keyboards.inline import get_language_keyboard
 from database.operations import get_or_create_user
+import json
 
-from bot.locales.utils import get_text
+with open("data/text_menu.json", "r", encoding="utf-8") as f:
+    texts = json.load(f)
+
 
 router = Router()
 
-@router.message(Command('start'))
+
+@router.message(Command("start"))
 async def start_handler(message: types.Message):
-    user =  await get_or_create_user(message.from_user.id) # type: ignore
-    await message.answer(get_text("welcome", user.language),
-        reply_markup=get_language_keyboard()
-        )
+    user = await get_or_create_user(message.from_user.id)  # type: ignore
+    user_name = message.from_user.first_name  # type: ignore
+    text = texts["menu"][user.language]["choose_language"].replace("{name}", user_name)
+    await message.answer(
+        text=text, reply_markup=get_language_keyboard(), parse_mode="HTML"
+    )
