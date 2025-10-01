a7873ad6cd30d0173599c5b7add47d1339a2d296
Vadym <vadymkravtcsov@gmail.com>
2025-10-01 23:30:48 +0300

    feat: add main keyboard import and stub for main menu

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,7 +1,7 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 
-from bot.keyboards.inline import get_language_keyboard
+from bot.keyboards.inline import get_language_keyboard, get_main_keyboard
 from database.operations import get_or_create_user
 import json
 
@@ -13,10 +13,15 @@ router = Router()
 
 
 @router.message(Command("start"))
-async def start_handler(message: types.Message):
+async def start_handler(message: types.Message) -> None:
     user = await get_or_create_user(message.from_user.id)  # type: ignore
     user_name = message.from_user.first_name  # type: ignore
+    # if not user.language:
     text = texts["menu"][user.language]["choose_language"].replace("{name}", user_name)
-    await message.answer(
-        text=text, reply_markup=get_language_keyboard(), parse_mode="HTML"
-    )
+    await message.answer(text=text, reply_markup=get_language_keyboard(), parse_mode="HTML")
+    # else:
+    #     await message.answer(  # type: ignore
+    #         text=texts["menu"][user.language]["main_menu_title"].replace("{name}", user_name),
+    #         reply_markup=get_main_keyboard(user.language),
+    #         parse_mode="HTML",
+    #     )
