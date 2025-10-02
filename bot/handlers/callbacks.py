f18cb165d4c8c4d110703dc2acd7848abf818bd9
Vadym <vadymkravtcsov@gmail.com>
2025-10-02 21:59:32 +0300

    feat: refactor callback handlers for clarity and bug fixes

--- bot/handlers/callbacks.py
+++ bot/handlers/callbacks.py
@@ -40,7 +40,7 @@ async def settings_language_handler(callback: types.CallbackQuery, lang: str):
 
 
 @router.callback_query(F.data.startswith("gender_"))
-async def gender_selection_handler(callback: types.CallbackQuery, state: FSMContext, lang: str ):
+async def gender_selection_handler(callback: types.CallbackQuery, state: FSMContext, lang: str):
     gender = callback.data.split("_")[1]  # type: ignore
     await UserService.update_user_field(callback.from_user.id, gender=gender)
     await callback.answer(text=texts["menu"][lang]["saved"])
@@ -68,9 +68,7 @@ async def birth_date_calendar_handler(callback: types.CallbackQuery, state: FSMC
     calendar = DialogCalendar(locale=lang)
     markup = await calendar.start_calendar(year=1990)
     await state.set_state(UserStates.waiting_for_birth_date)
-    await callback.message.edit_text(  # type: ignore
-        text=texts["menu"][lang]["choose_birth_date"], reply_markup=markup
-    )
+    await callback.message.edit_text(text=texts["menu"][lang]["choose_birth_date"], reply_markup=markup)  # type: ignore
 
 
 @router.callback_query(F.data == "set_birth_date")  # ⚙️
@@ -79,16 +77,20 @@ async def settings_birth_date_calendar_handler(callback: types.CallbackQuery, st
     calendar = DialogCalendar(locale=lang)
     markup = await calendar.start_calendar(year=1990)
     await state.set_state(UserStates.waiting_for_birth_date)
-    await callback.message.edit_text(  # type: ignore
-        text=texts["menu"][lang]["choose_birth_date"], reply_markup=markup
-    )
+    await callback.message.edit_text(text=texts["menu"][lang]["choose_birth_date"], reply_markup=markup)  # type: ignore
 
 
 @router.callback_query(DialogCalendarCallback.filter(), UserStates.waiting_for_birth_date)
 async def birth_data_save_handler(
     callback: types.CallbackQuery, callback_data: SimpleCalendarCallback, state: FSMContext, lang: str
 ):
-    user_name = callback.from_user.first_name
+    if callback.data is not None and "CANCEL" in callback.data:
+        await state.clear()
+        await callback.message.edit_text(  # type: ignore
+            text=texts["menu"][lang]["main_menu_title"], reply_markup=inline.get_main_keyboard(lang)
+        )
+        return None
+
     calendar = DialogCalendar(locale=lang)
     selected, date_selected = await calendar.process_selection(callback, callback_data)  # type: ignore
     if selected:
@@ -99,7 +101,7 @@ async def birth_data_save_handler(
         return_to = state_data.get("return_to", "main_menu")
         if return_to == "main_menu":
             await callback.message.edit_text(  # type: ignore
-                text=texts["menu"][lang]["main_menu_title"].replace("{name}", user_name),
+                text=texts["menu"][lang]["main_menu_title"],
                 reply_markup=inline.get_main_keyboard(lang),
                 parse_mode="HTML",
             )
@@ -133,20 +135,23 @@ async def user_settings(callback: types.CallbackQuery, lang: str):
 @router.callback_query(F.data.startswith("save_"))
 async def save_settings_handler(callback: types.CallbackQuery, lang: str):
     field = callback.data.split("_")[1]  # type: ignore
-    user_name = callback.from_user.first_name
     if field in ["ua", "en", "ru"]:
         await UserService.update_user_field(callback.from_user.id, language=field)
+        await callback.answer(text=texts["menu"][lang]["saved"])
+        await callback.message.edit_text(  # type: ignore
+            text=texts["menu"][field]["main_menu_title"],
+            reply_markup=inline.get_main_keyboard(field), parse_mode="HTML"
+            )
+        return None
+        
     elif field in ["male", "female"]:
         await UserService.update_user_field(callback.from_user.id, gender=field)
     else:
         await UserService.update_user_field(callback.from_user.id, birth_date=field)
     await callback.answer(text=texts["menu"][lang]["saved"])
     await callback.message.edit_text(  # type: ignore
-        text=texts["menu"][field]["main_menu_title"],
-        reply_markup=inline.get_main_keyboard(field),
-        parse_mode="HTML")
-        
-    
+        text=texts["menu"][lang]["main_menu_title"], reply_markup=inline.get_main_keyboard(lang), parse_mode="HTML"
+    )
 
 
 @router.callback_query(F.data == "in_dev")
