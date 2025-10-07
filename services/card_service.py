881a8c9274ddb4651878bad4d955decdabf5261b
Vadym <vadymkravtcsov@gmail.com>
2025-10-07 22:36:25 +0300

    feat: implement media ID caching logic in CardService

--- services/card_service.py
+++ services/card_service.py
@@ -7,17 +7,51 @@ import aiofiles
 import random
 import json
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class CardService:
     _lang_cashe: dict[str, dict[str, Any]] = {}
     _cashe_timestamps: dict[str, float] = {}
-    CACHE_TTL = 2000
+    _media_cashe: dict[str, dict[str, str]] = {}
+    CACHE_TTL = 3600
+
+    @classmethod
+    async def _load_media_ids(cls, deck_name: str):
+        file_path = f"data/media_ids/{deck_name}_ids.json"
+
+        try:
+            async with aiofiles.open(file_path, "r") as f:
+                content = await f.read()
+                media_ids = await asyncio.to_thread(json.loads, content)
+            cls._media_cashe[deck_name] = media_ids
+        except FileNotFoundError:
+            logger.info(f"❌ File ID for deck no found")
+            raise
+
+    @classmethod
+    async def get_media_id(cls, deck_name: str, card_number: str):
+        if deck_name not in cls._media_cashe:
+            await cls._load_media_ids(deck_name)
+        return cls._media_cashe[deck_name][card_number]
+
 
     @classmethod
     async def _load_language(cls, lang: str):
-        async with aiofiles.open(f"data/cards/{lang}.json", "r")as f:
+        async with aiofiles.open(f"data/cards/{lang}.json", "r") as f:
             content = await f.read()
             cards_descriptions = await asyncio.to_thread(json.loads, content)
 
         cls._lang_cashe[lang] = cards_descriptions["cards"]
         cls._cashe_timestamps[lang] = time.time()
+
+    @classmethod
+    async def get_card_descriptions(cls, card_number: str, lang: str):
+        if lang not in cls._lang_cashe:
+            await cls._load_language(lang)
+
+        if time.time() - cls._cashe_timestamps[lang] > cls.CACHE_TTL:
+            await cls._load_language(lang)
+
+        return cls._lang_cashe[lang][card_number]
