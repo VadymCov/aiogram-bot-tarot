d1b07ffea662ae91e498f285c5ffd1890540bd0d
Vadym <vadymkravtcsov@gmail.com>
2025-10-09 22:20:49 +0300

    feat: add in class User field - last+card_date

--- database/database.py
+++ database/database.py
@@ -1,10 +1,20 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from database.models import Base
-DATABASE_URL = "postgresql+asyncpg://user:pass@localhost/astro_bot"
+from config.settings import DATABASE_URL
+
+DATABASE_URL = DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
+
 async def init_db():
     async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# ❗❗❗ RESET DATABASE
+async def reset_db_once():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
