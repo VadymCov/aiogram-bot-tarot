08b1289a7e426861d86ab4d0a6357f8f92c0c480
Vadym <vadymkravtcsov@gmail.com>
2025-10-11 00:15:45 +0300

    feat: add the _load_language_media_ids class method that loads language images into the cache

--- services/card_service.py
+++ services/card_service.py
@@ -11,16 +11,17 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class CardService:
-    _lang_cache: dict[str, dict[str, Any]] = {}
+    _lang_desc_cache: dict[str, dict[str, Any]] = {}
     _cache_timestamps: dict[str, float] = {}
     _media_cache: dict[str, dict[str, str]] = {}
+    _lang_media_cache: dict[str, dict[str, str]] = {}
     CACHE_TTL = 3600
 
     @classmethod
     async def _load_media_ids(cls, deck_name: str):
         file_path = f"data/media_ids/{deck_name}_ids.json"
-
         try:
             async with aiofiles.open(file_path, "r", encoding="utf-8") as f:
                 content = await f.read()
@@ -36,22 +37,33 @@ class CardService:
             await cls._load_media_ids(deck_name)
         return cls._media_cache[deck_name][card_number]
 
-
     @classmethod
-    async def _load_language(cls, lang: str):
+    async def _load_language_desc(cls, lang: str):
         async with aiofiles.open(f"data/cards/{lang}.json", "r", encoding="utf-8") as f:
             content = await f.read()
             cards_descriptions = await asyncio.to_thread(json.loads, content)
 
-        cls._lang_cache[lang] = cards_descriptions["cards"]
+        cls._lang_desc_cache[lang] = cards_descriptions["cards"]
         cls._cache_timestamps[lang] = time.time()
 
     @classmethod
     async def get_card_descriptions(cls, card_number: str, lang: str):
-        if lang not in cls._lang_cache:
-            await cls._load_language(lang)
+        if lang not in cls._lang_desc_cache:
+            await cls._load_language_desc(lang)
 
         if time.time() - cls._cache_timestamps[lang] > cls.CACHE_TTL:
-            await cls._load_language(lang)
+            await cls._load_language_desc(lang)
 
-        return cls._lang_cache[lang][card_number]
+        return cls._lang_desc_cache[lang][card_number]
+
+    @classmethod
+    async def _load_language_media_ids(cls, deck_name, lang: str):
+        file_path = f"data/media_ids/lang_media_ids/{deck_name}/{lang}.json"
+        try:
+            async with aiofiles.open(file_path, "r") as f:
+                content = await f.read()
+                media_ids = await asyncio.to_thread(json.loads, content)
+            cls._lang_media_cache[deck_name][lang] = media_ids
+        except FileNotFoundError:
+            logger.exception(f"❌ File ID for deck {deck_name} and lang {lang} not found")
+            raise
