bd05afd933f05620973ff591107265dc6c0a3cbd
Vadym <vadymkravtcsov@gmail.com>
2025-10-10 21:25:54 +0300

    chore: fix typo in error message constant

--- services/card_service.py
+++ services/card_service.py
@@ -12,9 +12,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 class CardService:
-    _lang_cashe: dict[str, dict[str, Any]] = {}
-    _cashe_timestamps: dict[str, float] = {}
-    _media_cashe: dict[str, dict[str, str]] = {}
+    _lang_caсhe: dict[str, dict[str, Any]] = {}
+    _caсhe_timestamps: dict[str, float] = {}
+    _media_caсhe: dict[str, dict[str, str]] = {}
     CACHE_TTL = 3600
 
     @classmethod
@@ -22,36 +22,36 @@ class CardService:
         file_path = f"data/media_ids/{deck_name}_ids.json"
 
         try:
-            async with aiofiles.open(file_path, "r") as f:
+            async with aiofiles.open(file_path, "r", encoding="utf-8") as f:
                 content = await f.read()
                 media_ids = await asyncio.to_thread(json.loads, content)
-            cls._media_cashe[deck_name] = media_ids
+            cls._media_caсhe[deck_name] = media_ids
         except FileNotFoundError:
             logger.info(f"❌ File ID for deck no found")
             raise
 
     @classmethod
     async def get_media_id(cls, deck_name: str, card_number: str):
-        if deck_name not in cls._media_cashe:
+        if deck_name not in cls._media_caсhe:
             await cls._load_media_ids(deck_name)
-        return cls._media_cashe[deck_name][card_number]
+        return cls._media_caсhe[deck_name][card_number]
 
 
     @classmethod
     async def _load_language(cls, lang: str):
-        async with aiofiles.open(f"data/cards/{lang}.json", "r") as f:
+        async with aiofiles.open(f"data/cards/{lang}.json", "r", encoding="utf-8") as f:
             content = await f.read()
             cards_descriptions = await asyncio.to_thread(json.loads, content)
 
-        cls._lang_cashe[lang] = cards_descriptions["cards"]
-        cls._cashe_timestamps[lang] = time.time()
+        cls._lang_caсhe[lang] = cards_descriptions["cards"]
+        cls._caсhe_timestamps[lang] = time.time()
 
     @classmethod
     async def get_card_descriptions(cls, card_number: str, lang: str):
-        if lang not in cls._lang_cashe:
+        if lang not in cls._lang_caсhe:
             await cls._load_language(lang)
 
-        if time.time() - cls._cashe_timestamps[lang] > cls.CACHE_TTL:
+        if time.time() - cls._caсhe_timestamps[lang] > cls.CACHE_TTL:
             await cls._load_language(lang)
 
-        return cls._lang_cashe[lang][card_number]
+        return cls._lang_caсhe[lang][card_number]
