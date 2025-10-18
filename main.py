bf6c42bb7aa8c9f0579d3f8767e226e7e4ccef72
Vadym <vadymkravtcsov@gmail.com>
2025-10-18 23:03:38 +0300

    add configure logging formatter

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from setup import preload_card, setup_dispatcher
 
 from config.settings import BOT_TOKEN
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname) - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 logger = logging.getLogger(__name__)
 
 
